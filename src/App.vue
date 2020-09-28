0e67936bfee80b6b1c3b7f8f233e0cbe80531922
amir78729 <amir78729@gmail.com>
2020-09-28 21:33:39 +0330

    scrollpane modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app" :class="{'dark': isDark,'light': !isDark}" class="container-fluid" >
     <button class="btn no-focus " @click="toggleTheme"><i style="font-size: 15px " class="rounded-corner material-icons">brightness_medium</i></button>
-    <hr>
     <button class="btn no-focus buttons-30x30 rounded-corner" :style="{backgroundColor: colors.red}" @click="changeColor('red')"></button>
     <button class="btn no-focus buttons-30x30 rounded-corner" :style="{backgroundColor: colors.yellow}" @click="changeColor('yellow')"></button>
     <button class="btn no-focus buttons-30x30 rounded-corner" :style="{backgroundColor: colors.green}" @click="changeColor('green')"></button>
@@ -10,6 +9,18 @@
     <hr>
     <h1 :style="{color: currentColor}">{{currentColor}}</h1>
     <p>hi</p>
+
+
+
+    <div class="title">
+
+    </div>
+    <div class="secondary container-fluid " style="margin: 0">
+      hi
+    </div>
+    <div class="title">
+
+    </div>
 
   </div>
 </template>
@@ -54,7 +65,7 @@ export default {
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     /*width: 100%;*/
-    padding: 10px;
+    padding: 0px;
     margin: 0;
     /*transition: all .3s;*/
   }
@@ -75,6 +86,19 @@ export default {
     width: 30px;
   }
 
+  .title{
+    background-image: url("https://cdn.pixabay.com/photo/2018/10/30/16/06/water-lily-3784022_1280.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    height: 400px;
+    width: 100%;
+  }
+  .secondary{
+    padding: 0;
+  }
+
   /******************************************************************************/
 
   .dark{
@@ -84,6 +108,16 @@ export default {
 
   .light{
     background-color: #efefef;
+    color: #202020;
+  }
+
+  .dark .secondary{
+    background-color: #303030;
+    color: #B0B0B0;
+  }
+
+  .light .secondary{
+    background-color: #dedede;
     color: #202020;
   }
 
@@ -111,4 +145,30 @@ export default {
     background-color: #202020;
     transform: rotate(180deg);
   }
+
+  /*********************/
+  /* width */
+  ::-webkit-scrollbar {
+    width: 8px;
+    height: 5px;
+    transition: .5s;
+  }
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background-color: #333333;
+    transition: .5s;
+    /*box-shadow: inset 0 0 5px grey;*/
+  }
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #555555;
+    transition: .5s;
+    /*border-radius: 10px;*/
+  }
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #666666;
+    transition: .5s;
+  }
+
 </style>
